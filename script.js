4de6e120b536dd1ac73f1bc032c6af72e18aec0f
Trần Tuấn Trường <42940093+truongsofm@users.noreply.github.com>
2019-07-12 17:03:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// Link nhạc nền - cho bằng null nếu không muốn nhạc nền
+var bgMusicURL = 'https://github.com/truongsofm/music/blob/master/GapEmDungLuc-LuanTang-4934857_hq.mp3?raw=true';
+var bgMusicControls = false; // Hiện khung điều khiển nhạc nền hay không - chọn true hoặc false (truongsofm)
